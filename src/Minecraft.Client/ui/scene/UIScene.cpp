1cf56c96cfab35070e8756f90b8eb6ece48d35eb
Bagietka <bagietabusiness@gmail.com>
2025-03-01 18:16:39 +0100

    small fixes

--- src/Minecraft.Client/ui/scene/UIScene.cpp
+++ src/Minecraft.Client/ui/scene/UIScene.cpp
@@ -6,8 +6,8 @@
 // It's matching but... some of those variable inits should be moved to header
 UIScene::UIScene(int padID, UILayer* uiLayer){
     this->mPadID = padID;
-    this->qword30 = 0LL;
-    this->mFuiFile = 0LL;
+    this->qword30 = nullptr;
+    this->mFuiFile = nullptr;
     this->mFocusRelated = 0;
     this->mHideLowerScenes = 0;
     this->byte_e9 = 0;
@@ -22,8 +22,8 @@ UIScene::UIScene(int padID, UILayer* uiLayer){
     this->byte_ea = 1;
     this->dwordF8 = 0;
     this->mUILayer = uiLayer;
-    this->mBackScene = 0LL;
-    this->mCallbackUniqueId = 0LL;
+    this->mBackScene = nullptr;
+    this->mCallbackUniqueId = nullptr;
     this->wstring_8 = L"";
     this->bool_28 = 0;
 }
