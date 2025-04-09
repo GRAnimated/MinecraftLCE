3f5ea631ba1b6e29e7adaff13b6f8c0c66ddb4d9
Bagietka <bagietabusiness@gmail.com>
2025-04-09 18:25:53 +0200

    Recipes & MappedRegistry

--- src/Minecraft.Core/MappedRegistry.h
+++ src/Minecraft.Core/MappedRegistry.h
@@ -3,7 +3,8 @@
 #include "Minecraft.Core/IdMap.h"
 
 template <typename Key, typename Value, typename Hash = std::hash<Key>, typename KeyEqual = std::equal_to<Key>, typename HashValue = std::hash<Value>, typename ValueEqual = std::equal_to<Value>>
-class MappedRegistry : public SimpleRegistry<Key, Value>, IdMap<Value>{
+class MappedRegistry : public SimpleRegistry<Key, Value>, public IdMap<Value>{
 public:
-    virtual void registerMapping(int id, const Key& key, const Value& value);
+    virtual void shit();
+    virtual void registerMapping(int id, Key key, Value value);
 };
--- src/Minecraft.World/item/crafting/Recipes.h
+++ src/Minecraft.World/item/crafting/Recipes.h
@@ -6,5 +6,6 @@ class Recipes{
 public:
     Recipes();
     void addShapedRecipe(ItemInstance*, ...);
+    void addShapelessRecipe(ItemInstance*, ...);
     void buildRecipeIngredientsArray();
 };
