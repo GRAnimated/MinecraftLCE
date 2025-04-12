b69f299812f9a4126972dcef58e14971d9d61c03
Dexrn ZacAttack <dexrn@hotmail.com>
2025-04-12 14:49:56 -0700

    Decompiled VitaVirtualDecompress

--- src/Minecraft.Client/Compression.h
+++ src/Minecraft.Client/Compression.h
@@ -4,4 +4,11 @@ class Compression {
 public:
     static void CreateNewThreadStorage();
     static void ReleaseThreadStorage();
+
+    /// @brief Decompresses "Vita RLE"
+    /// @param out Output buffer to store decompressed data
+    /// @param sizeOut Pointer to uint to store the size of the decompressed data
+    /// @param in Pointer to the input compressed data
+    /// @param sizeIn The size of the input data
+    static void VitaVirtualDecompress(void* out, uint* sizeOut, void* in, uint sizeIn);
 };
