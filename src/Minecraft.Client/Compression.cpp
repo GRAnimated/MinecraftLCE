394ee3e6050b3cdac4335da3c6ba2507c109743f
Dexrn ZacAttack <dexrn@hotmail.com>
2025-04-13 23:47:35 -0700

    Decompiled internalDecompressRle (w/ help)

--- src/Minecraft.Client/Compression.cpp
+++ src/Minecraft.Client/Compression.cpp
@@ -1,10 +1,11 @@
 #include <types.h>
 #include "Compression.h"
+#include <cstdint>
 
-void Compression::VitaVirtualDecompress(void* dest, uint* destSize, void* src, uint srcSize) {
+void Compression::VitaVirtualDecompress(void* dst, uint* dstSize, void* src, uint srcSize) {
     uchar* srcPtr = (uchar*)src;
-    uchar* destPtr = (uchar*)dest;
-    int destOffset = 0;
+    uchar* dstPtr = (uchar*)dst;
+    int dstOffset = 0;
     int index = 0;
 
     // while we haven't reached the end of the input data
@@ -12,18 +13,63 @@ void Compression::VitaVirtualDecompress(void* dest, uint* destSize, void* src, u
         // check if the current byte is not zero
         if (srcPtr[index]) {
             // if it is not zero, copy the byte to the output buffer
-            destPtr[destOffset++] = srcPtr[index];
+            dstPtr[dstOffset++] = srcPtr[index];
         } else {
             // read the count
             uint count = srcPtr[++index];
 
             // write `count` amount of zeroes to the output buffer
             for (int z = 0; z < count; ++z) {
-                destPtr[destOffset++] = 0;
+                dstPtr[dstOffset++] = 0;
             }
         }
     }
     
-    // set the destSize to how many bytes we wrote to the output buffer
-    *destSize = destOffset;
+    // set the dstSize to how many bytes we wrote to the output buffer
+    *dstSize = dstOffset;
+}
+
+bool Compression::internalDecompressRle(void* dst, unsigned int* dstSize, void* src, unsigned int srcSize) {
+    unsigned char* dst8 = static_cast<unsigned char*>(dst);
+    unsigned char* src8 = static_cast<unsigned char*>(src);
+    unsigned char* dstOffset = dst8;
+
+    // if srcSize isn't 0 (because otherwise there's nothing to do)
+    if (srcSize != 0) {
+        // get the end of the input buffer
+        unsigned char* srcEnd = src8 + srcSize;
+        // while we haven't reached the end of the input buffer
+        while (src8 != srcEnd) {
+            // if the current byte is 0xff, we should decompress
+            unsigned char val = *src8++;
+            if (val == 0xFF) {
+                // compressed
+                unsigned char count = *src8++;
+                unsigned char repeatValue;
+
+                // if count is bigger than 3, we should repeat the next byte count times
+                // reason (likely) being is that it's not making the file smaller when taking up 3 bytes of space for just 2-3 repeated bytes
+                // also the logic also probably allows for files containing 0xFF to not break the compression. (untested)
+                if (count < 3) {
+                    repeatValue = 0xff;
+                } else {
+                    repeatValue = *src8++;
+                }
+
+                // write value count times
+                for (int i = 0; i <= count; ++i) {
+                    *dstOffset++ = repeatValue;
+                }
+            } else {
+                // write normal byte
+                *dstOffset++ = val;
+            }
+        }
+    }
+
+    // set the dstSize to how many bytes we wrote to the output buffer
+    *dstSize = dstOffset - dst8;
+
+    // thanks 4J for making this specific decompression function return bool, also it always returns false... why?
+    return false;
 }
--- src/Minecraft.Client/Compression.h
+++ src/Minecraft.Client/Compression.h
@@ -10,5 +10,13 @@ public:
     /// @param sizeOut Pointer to uint to store the size of the decompressed data
     /// @param in Pointer to the input compressed data
     /// @param sizeIn The size of the input data
-    static void VitaVirtualDecompress(void* dest, uint* destSize, void* src, uint srcSize);
+    static void VitaVirtualDecompress(void* dst, uint* dstSize, void* src, uint srcSize);
+
+    /// @brief Decompresses Internal RLE, mainly used for chunks.
+    /// @param out Output buffer to store decompressed data
+    /// @param sizeOut Pointer to uint to store the size of the decompressed data
+    /// @param in Pointer to the input compressed data
+    /// @param sizeIn The size of the input data
+    /// @returns `false` if there was no error, otherwise `true` (assumed, in reality it always returns `false`)
+    static bool internalDecompressRle(void* dst, uint* dstSize, void* src, uint srcSize);
 };
