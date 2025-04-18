dbc9dd24e18c4a1c69c4249fed3f4e605d595a81
GRAnimated <devin@nhscan.com>
2025-04-18 01:35:58 -0400

    Implement 7 packets

---
+++ src/Minecraft.Network/protocol/game/ClientboundSetCarriedItemPacket.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Minecraft.Network/protocol/Packet.h"
+
+class ClientboundSetCarriedItemPacket : public Packet,
+                                        public std::enable_shared_from_this<ClientboundSetCarriedItemPacket> {
+public:
+    static std::shared_ptr<Packet> create();
+
+    ClientboundSetCarriedItemPacket();
+    int getEstimatedSize() override;
+    EPacketType getPacketId() override;
+    void read(DataInputStream* input) override;
+    void write(DataOutputStream* output) override;
+    void handle(PacketListener* listener) override;
+
+private:
+    int itemId;
+};
---
+++ src/Minecraft.Network/protocol/game/ServerboundContainerClosePacket.cpp
@@ -0,0 +1,27 @@
+#include "Minecraft.Network/protocol/game/ServerboundContainerClosePacket.h"
+
+#include "Minecraft.Client/multiplayer/PacketListener.h"
+#include "Minecraft.Core/io/DataInputStream.h"
+#include "Minecraft.Core/io/DataOutputStream.h"
+
+std::shared_ptr<Packet> ServerboundContainerClosePacket::create() {
+    return std::shared_ptr<Packet>(new ServerboundContainerClosePacket());
+}
+
+ServerboundContainerClosePacket::ServerboundContainerClosePacket() {}
+
+EPacketType ServerboundContainerClosePacket::getPacketId() {
+    return EPacketType::_ServerboundContainerClosePacket;
+}
+
+void ServerboundContainerClosePacket::read(DataInputStream* input) {
+    containerId = input->readByte();
+}
+
+void ServerboundContainerClosePacket::write(DataOutputStream* output) {
+    output->writeByte(containerId);
+}
+
+void ServerboundContainerClosePacket::handle(PacketListener* listener) {
+    listener->handleContainerClose(shared_from_this());
+}
