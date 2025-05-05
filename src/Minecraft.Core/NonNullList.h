27b939e7c6a924edd07471d4c48425d3c39b2125
Bagietka <bagietabusiness@gmail.com>
2025-05-05 18:44:43 +0200

    NonNullList better comment

--- src/Minecraft.Core/NonNullList.h
+++ src/Minecraft.Core/NonNullList.h
@@ -1,6 +1,5 @@
-#include "types.h"
 #include "Minecraft.World/ArrayWithLength.h"
 template <typename T>
 class NonNullList : arrayWithLength<T> {
-    T baseListItem;  // idk
+    T baseListItem;  // idk if this should be not_null_ptr by default or specified in template
 };
