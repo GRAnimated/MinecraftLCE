3981ad7dfa493bc42833d73c180caeea8fa7fac2
Dexrn ZacAttack <dexrn@hotmail.com>
2025-08-27 01:48:40 -0700

    match ServerboundPreLoginPacket

--- src/Minecraft.Client/net/minecraft/network/protocol/game/ServerboundPreLoginPacket.h
+++ src/Minecraft.Client/net/minecraft/network/protocol/game/ServerboundPreLoginPacket.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "net/minecraft/network/protocol/Packet.h"
+#include <string>
 
 // PACKET STUB
 class ServerboundPreLoginPacket : public Packet,
@@ -8,10 +9,14 @@ class ServerboundPreLoginPacket : public Packet,
 public:
     static std::shared_ptr<Packet> create();
 
-    // TODO: constructor(s)
+    ServerboundPreLoginPacket(const std::wstring &name);
+    ServerboundPreLoginPacket();
 
     EPacketType getPacketId() override;
     void read(DataInputStream* input) override;
     void write(DataOutputStream* output) override;
     void handle(PacketListener* listener) override;
+
+    short mProtocolVersion;
+    std::wstring mName;
 };
