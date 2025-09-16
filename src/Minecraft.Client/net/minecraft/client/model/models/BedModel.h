2f61d1c5d6732f4ac11ac4f0460b60662fbe2e71
Bagietas <121623949+Bagietas@users.noreply.github.com>
2025-09-16 13:32:05 +0200

    Implement `BedModel` (#77)

---
+++ src/Minecraft.Client/net/minecraft/client/model/models/BedModel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "net/minecraft/client/model/Model.h"
+#include "net/minecraft/client/model/ModelDefinition.h"
+
+class BedModel : public Model {
+public:
+    BedModel(const ModelDefinition&);
+
+    void render(bool);  // this is what 4J done :)
+
+    std::wstring getName() override;
+
+    ModelPartSkin* mBedPart;
+    ModelPartSkin* mLegPart;
+};
