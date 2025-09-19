8173df1b5fffc6579443553a4ff57ae41277f02b
AzaleaCatgirl99 🌸 <34215218+AzaleaCatgirl99@users.noreply.github.com>
2025-09-19 17:01:12 -0400

    Implement most of BiomeSource, ChunkPos, & a few small things (#78)

---
+++ src/Minecraft.World/net/minecraft/world/level/biome/BiomeCache.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "net/minecraft/world/ArrayWithLength.h"
+#include <unordered_map>
+
+class BiomeSource;
+class Biome;
+
+class BiomeCache {
+public:
+    class Area;
+
+    BiomeCache(BiomeSource* biomeSource);
+    ~BiomeCache();
+
+    Biome* getBiome(int, int, Biome*);
+    arrayWithLength<Biome*> getBiomeBlockAt(int, int);
+    arrayWithLength<unsigned char> getBiomeIndexBlockAt(int, int);
+    Area* getBlockAt(int, int);
+    void update();
+
+    BiomeSource* mBiomeSource;
+    long long field_8;
+    std::unordered_map<long long, Area*> field_10;
+    std::vector<Area*> mSlots;
+    uint8_t skip[0x20];
+};
+
+static_assert(sizeof(BiomeCache) == 0x70);
