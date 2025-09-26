c8e83d2ecb127e17b5a1512e1ce12ad1dcef2858
Bagietas <121623949+Bagietas@users.noreply.github.com>
2025-09-26 17:14:06 +0200

    another format

--- src/Minecraft.Client/net/minecraft/client/Minecraft.cpp
+++ src/Minecraft.Client/net/minecraft/client/Minecraft.cpp
@@ -114,7 +114,9 @@ void Minecraft::init() {
 
     // this is cursed why would you do this
     MemSect(31);
-    { std::wstring status(L"Pre startup"); }
+    {
+        std::wstring status(L"Pre startup");
+    }
     MemSect(0);
 
     GlStateManager::enableTexture();
@@ -130,7 +132,9 @@ void Minecraft::init() {
     GlStateManager::matrixMode(0);
 
     MemSect(31);
-    { std::wstring status(L"Startup"); }
+    {
+        std::wstring status(L"Startup");
+    }
     MemSect(0);
 
     mLevelRenderer = new LevelRenderer(this, mTextures);
@@ -140,7 +144,9 @@ void Minecraft::init() {
     mParticleEngine = new ParticleEngine(mLevel, mTextures);
 
     MemSect(31);
-    { std::wstring status(L"Post startup"); }
+    {
+        std::wstring status(L"Post startup");
+    }
     MemSect(0);
 
     mGui = new Gui(this);
