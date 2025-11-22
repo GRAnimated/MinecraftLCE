815e028e9f828d52ed90513af72832b34072e28c
Bagietas <121623949+Bagietas@users.noreply.github.com>
2025-11-22 20:31:46 +0100

    Implement `LevelType` (#91)

---
+++ src/Minecraft.World/net/minecraft/world/level/LevelType.cpp
@@ -0,0 +1,80 @@
+#include "net/minecraft/world/level/LevelType.h"
+
+LevelType* LevelType::NORMAL;
+LevelType* LevelType::FLAT;
+LevelType* LevelType::LARGE_BIOMES;
+LevelType* LevelType::AMPLIFIED;
+LevelType* LevelType::CUSTOMIZED;
+LevelType* LevelType::NORMAL_1_1;
+
+LevelType* LevelType::TYPES[16];
+
+LevelType::LevelType(int id, const std::wstring& name, int version) {
+    this->_init(id, name, version);
+}
+
+LevelType::LevelType(int id, const std::wstring& name) {
+    this->_init(id, name, 0);
+}
+
+void LevelType::_init(int id, const std::wstring& name, int version) {
+    this->mId = id;
+    this->mHasHelpText = false;
+    this->mName = name;
+    this->mVersion = version;
+    this->mSelectableByUser = true;
+
+    LevelType::TYPES[id] = this;
+}
+
+std::wstring LevelType::getGeneratorName() {
+    return this->mName;
+}
+
+int LevelType::getVersion() {
+    return this->mVersion;
+}
+
+LevelType* LevelType::setHasReplacement() {
+    this->mHasReplacement = true;
+    return this;
+}
+
+LevelType* LevelType::setHasHelpText() {
+    this->mHasHelpText = true;
+    return this;
+}
+
+LevelType* LevelType::setSelectableByUser(bool state) {
+    this->mSelectableByUser = state;
+    return this;
+}
+
+void LevelType::staticCtor() {
+    LevelType::TYPES[0] = nullptr;
+    LevelType::TYPES[1] = nullptr;
+    LevelType::TYPES[2] = nullptr;
+    LevelType::TYPES[3] = nullptr;
+    LevelType::TYPES[4] = nullptr;
+    LevelType::TYPES[5] = nullptr;
+    LevelType::TYPES[6] = nullptr;
+    LevelType::TYPES[7] = nullptr;
+    LevelType::TYPES[8] = nullptr;
+    LevelType::TYPES[9] = nullptr;
+    LevelType::TYPES[10] = nullptr;
+    LevelType::TYPES[11] = nullptr;
+    LevelType::TYPES[12] = nullptr;
+    LevelType::TYPES[13] = nullptr;
+    LevelType::TYPES[14] = nullptr;
+    LevelType::TYPES[15] = nullptr;
+
+    LevelType::NORMAL = (new LevelType(0, L"default", 1))->setHasReplacement();
+    LevelType::FLAT = new LevelType(1, L"flat");
+    LevelType::LARGE_BIOMES = new LevelType(2, L"largeBiomes");
+    LevelType::AMPLIFIED = (new LevelType(3, L"amplified"))->setHasHelpText();
+    LevelType::CUSTOMIZED = new LevelType(4, L"customized");
+
+    // where are other 3 types
+
+    LevelType::NORMAL_1_1 = (new LevelType(8, L"default_1_1", 0))->setSelectableByUser(false);
+}
--- src/Minecraft.World/net/minecraft/world/level/LevelType.h
+++ src/Minecraft.World/net/minecraft/world/level/LevelType.h
@@ -11,8 +11,26 @@ public:
     static LevelType* CUSTOMIZED;
     static LevelType* NORMAL_1_1;
 
+    static LevelType* TYPES[16];
+
+    LevelType(int id, const std::wstring& name, int version);
+    LevelType(int id, const std::wstring& name);
+
+    void _init(int id, const std::wstring& name, int version);
+
     std::wstring getGeneratorName();
     int getVersion();
 
+    LevelType* setHasReplacement();
+    LevelType* setHasHelpText();
+    LevelType* setSelectableByUser(bool state);
+
     static void staticCtor();
+
+    int mId;
+    std::wstring mName;
+    int mVersion;
+    bool mSelectableByUser;
+    bool mHasReplacement;
+    bool mHasHelpText;
 };
