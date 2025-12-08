8becca08890e41258faee89ff14e8537d4b81bfa
Bagietas <121623949+Bagietas@users.noreply.github.com>
2025-12-08 01:24:57 +0100

    Implement `EnumProperty` constructor (#93)
    * Implement `MonsterEggBlock`, `PrismarineBlock`, `StoneBrickBlock`
    
    * oops
    
    * Update data_symbols.csv
    
    * Update MonsterEggBlock.cpp
    
    * try to match `EnumProperty::create`
    
    * linter
    
    * Match another EnumProperty func
    
    * Update EnumProperty.h
    
    * Implement `EnumProperty` constructor
    
    * another pile of crap

--- src/Minecraft.World/net/minecraft/world/level/block/state/properties/AbstractProperty.h
+++ src/Minecraft.World/net/minecraft/world/level/block/state/properties/AbstractProperty.h
@@ -7,8 +7,11 @@
 template <typename T>
 class Predicate {
 public:
+    Predicate() {}
+
     virtual bool apply(T) const = 0;
-    virtual Predicate<T>* copy() const;
+    virtual Predicate<T>* copy() const { return nullptr; }
+    virtual ~Predicate() {}
 };
 
 template <typename T>
@@ -26,15 +29,18 @@ public:
             return new FunctionPredicate(predicate);
         }
 
-        bool apply(T) const override;
+        bool apply(T a) const override { return this->mPredicate(a); }
 
         bool (*mPredicate)(T);
     };
     class ConstantPredicate : public Predicate<T> {
     public:
-        ConstantPredicate(bool);
+        ConstantPredicate(bool constValue) { this->mConstValue = constValue; }
 
-        bool apply(T) const override;
+        bool apply(T) const override { return this->mConstValue; }
+        Predicate<T>* copy() const override { return new ConstantPredicate(this->mConstValue); }
+
+        bool mConstValue;  // I guess it's bool, could be union; if you find possiblity of it, do it
     };
     class EqualToValuePredicate : public Predicate<T> {
         bool apply(T) const override;
@@ -43,6 +49,10 @@ public:
 
 class Boxed;
 class Property {
+public:
+    Property();
+    void updateCachedHashCode();
+
     virtual std::wstring getName() const = 0;
     virtual std::wstring getName(const Boxed*) const = 0;
     virtual const std::vector<Boxed*>& getPossibleValues() const = 0;
@@ -59,6 +69,9 @@ class Property {
 template <typename T>
 class AbstractProperty : public Property {
 public:
+    AbstractProperty(const std::wstring& name, const std::type_info& typeInfo)
+        : Property(), mTypeInfo(typeInfo), mName(name) {}
+
     std::wstring getName() const override;
     std::wstring getName(const Boxed*) const override;
     const std::vector<Boxed*>& getPossibleValues() const override;
@@ -69,4 +82,8 @@ public:
     int hashCode() const override;
     virtual std::wstring getName(const T&) const = 0;  // ?
     virtual T getUnboxedValue(const std::wstring&) const = 0;
+
+    void* fill;
+    const std::type_info& mTypeInfo;
+    std::wstring mName;
 };
