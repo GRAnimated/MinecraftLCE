916a14cdb6b691ee685d7439e68a0dba80d9ff07
Bagietas <121623949+Bagietas@users.noreply.github.com>
2025-12-30 21:13:36 +0100

    Implement `PumpkinFeature`

---
+++ src/Minecraft.World/net/minecraft/world/level/levelgen/feature/PumpkinFeature.cpp
@@ -0,0 +1,27 @@
+#include "net/minecraft/core/BlockPos.h"
+#include "net/minecraft/core/Direction.h"
+#include "net/minecraft/world/Random.h"
+#include "net/minecraft/world/level/Level.h"
+#include "net/minecraft/world/level/block/Blocks.h"
+#include "net/minecraft/world/level/block/PumpkinBlock.h"
+#include "net/minecraft/world/level/block/state/BlockState.h"
+#include "net/minecraft/world/level/levelgen/feature/Feature.h"
+#include "net/minecraft/world/level/levelgen/feature/PumpkinFeature.h"
+
+PumpkinFeature::PumpkinFeature() : Feature(false) {}
+
+bool PumpkinFeature::place(Level* level, Random& rand, const BlockPos& pos) {
+    for (int i = 0; i < 64; ++i) {
+        BlockPos temp = pos.offset(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4),
+                                   rand.nextInt(8) - rand.nextInt(8));
+        if (level->isEmptyBlock(temp) && level->getBlockState(temp.below())->getBlock() == Blocks::GRASS
+            && Blocks::PUMPKIN->mayPlace(level, temp)) {
+            level->setBlock(temp,
+                            Blocks::PUMPKIN->defaultBlockState()->setValue(
+                                PumpkinBlock::AXIS, Direction::Plane::HORIZONTAL->getRandomDirection(rand)),
+                            2, false);
+        }
+    }
+
+    return true;
+}
---
+++ src/Minecraft.World/net/minecraft/world/level/levelgen/feature/PumpkinFeature.h
@@ -0,0 +1,7 @@
+#include "net/minecraft/world/level/levelgen/feature/Feature.h"
+class PumpkinFeature : public Feature {
+public:
+    PumpkinFeature();
+
+    bool place(Level* level, Random& random, const BlockPos& pos) override;
+};
